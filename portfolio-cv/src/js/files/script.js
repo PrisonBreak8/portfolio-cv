0a39cfae5174e87233ddb21f5be97acd2d3f4589
Artur <golubev.artur.1986@gmail.com>
2022-12-07 19:07:01 +0300

    fix: section portfolio

--- portfolio-cv/src/js/files/script.js
+++ portfolio-cv/src/js/files/script.js
@@ -91,73 +91,10 @@ if (list) {
 	}
 }
 //========================================================================================================================================================
-/*
-const canvar = document.querySelector('.canvas');
-if (canvar) {
-	window.onload = function () {
-		//get the canvas and conext and store in vars
-		var canvas = document.getElementById("sky");
-		var ctx = canvas.getContext("2d");
 
-		var W = window.innerWidth;
-		var H = window.innerHeight;
-		canvas.width = W;
-		canvas.height = H;
 
-		var mf = 100;
-		var flakes = [];
 
-		for (var i = 0; i < mf; i++) {
-			flakes.push({
-				x: Math.random() * W,
-				y: Math.random() * H,
-				r: Math.random() * 0.5 + 0.5,
-				d: Math.random() + 0.5
-			});
-		}
-
-		function drawFlakes() {
-			ctx.clearRect(0, 0, W, H);
-			ctx.fillStyle = "white";
-			ctx.beginPath();
-			for (var i = 0; i < mf; i++) {
-				var f = flakes[i];
-				ctx.moveTo(f.x, f.y);
-				ctx.arc(f.x, f.y, f.r, 0, Math.PI * 2, true);
-			}
-			ctx.fill();
-			moveFlakes();
-		}
-
-		var angle = 0;
-
-		function moveFlakes() {
-			angle += 0.01;
-			for (var i = 0; i < mf; i++) {
-				var f = flakes[i];
-
-				f.y += Math.pow(f.d, 2) * 1;
-				f.x += Math.sin(angle) * 2;
-
-				if (f.y > H) {
-					flakes[i] = {
-						x: Math.random() * W,
-						y: 0,
-						r: f.r,
-						d: f.d
-					}
-				}
-			}
-
-		}
-
-		setInterval(drawFlakes, 25);
-	}
-}
-*/
-//========================================================================================================================================================
-
-//========================================================================================================================================================
+//=========== RUBBER BAND Text Animation =============================================================================================================================================
 const rubAnim = document.querySelectorAll('.label__pop');
 if (rubAnim) {
 	for (let i = 0; i < rubAnim.length; i++) {
@@ -170,3 +107,5 @@ if (rubAnim) {
 	}
 }
 //========================================================================================================================================================
+
+
