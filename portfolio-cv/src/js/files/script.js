17298048cf3084853327ff616902db8496bc63eb
Artur <golubev.artur.1986@gmail.com>
2022-12-07 11:02:15 +0300

    feat: fix hero section

--- portfolio-cv/src/js/files/script.js
+++ portfolio-cv/src/js/files/script.js
@@ -35,6 +35,9 @@ themeButton.addEventListener('click', () => {
 	localStorage.setItem('selected-icon', getCurrentIcon())
 });
 //========================================================================================================================================================
+
+
+
 //=========== FILTERS =============================================================================================================================================
 const list = document.querySelector('.list');
 const items = document.querySelectorAll('.portfolio__item');
@@ -87,3 +90,83 @@ if (list) {
 		})
 	}
 }
+//========================================================================================================================================================
+/*
+const canvar = document.querySelector('.canvas');
+if (canvar) {
+	window.onload = function () {
+		//get the canvas and conext and store in vars
+		var canvas = document.getElementById("sky");
+		var ctx = canvas.getContext("2d");
+
+		var W = window.innerWidth;
+		var H = window.innerHeight;
+		canvas.width = W;
+		canvas.height = H;
+
+		var mf = 100;
+		var flakes = [];
+
+		for (var i = 0; i < mf; i++) {
+			flakes.push({
+				x: Math.random() * W,
+				y: Math.random() * H,
+				r: Math.random() * 0.5 + 0.5,
+				d: Math.random() + 0.5
+			});
+		}
+
+		function drawFlakes() {
+			ctx.clearRect(0, 0, W, H);
+			ctx.fillStyle = "white";
+			ctx.beginPath();
+			for (var i = 0; i < mf; i++) {
+				var f = flakes[i];
+				ctx.moveTo(f.x, f.y);
+				ctx.arc(f.x, f.y, f.r, 0, Math.PI * 2, true);
+			}
+			ctx.fill();
+			moveFlakes();
+		}
+
+		var angle = 0;
+
+		function moveFlakes() {
+			angle += 0.01;
+			for (var i = 0; i < mf; i++) {
+				var f = flakes[i];
+
+				f.y += Math.pow(f.d, 2) * 1;
+				f.x += Math.sin(angle) * 2;
+
+				if (f.y > H) {
+					flakes[i] = {
+						x: Math.random() * W,
+						y: 0,
+						r: f.r,
+						d: f.d
+					}
+				}
+			}
+
+		}
+
+		setInterval(drawFlakes, 25);
+	}
+}
+*/
+//========================================================================================================================================================
+
+//========================================================================================================================================================
+const rubAnim = document.querySelectorAll('.label__pop');
+if (rubAnim) {
+	for (let i = 0; i < rubAnim.length; i++) {
+		rubAnim[i].addEventListener('mouseenter', function () {
+			rubAnim[i].classList.add('rubberBand');
+			setTimeout(function () {
+				rubAnim[i].classList.remove('rubberBand');
+			}, 1000);
+		})
+	}
+}
+//========================================================================================================================================================
