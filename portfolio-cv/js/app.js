d458e0015161e4692d62bb1f6e1c6581db087fff
Artur <golubev.artur.1986@gmail.com>
2022-12-10 11:22:47 +0300

    fix: margin title

--- portfolio-cv/js/app.js
+++ portfolio-cv/js/app.js
@@ -734,7 +734,7 @@
         }));
         const root = document.documentElement;
         const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
-        const marqueeContent = document.querySelector("ul.marquee-content");
+        const marqueeContent = document.querySelector("ul.marquee__content");
         if (marqueeContent) {
             root.style.setProperty("--marquee-elements", marqueeContent.children.length);
             for (let i = 0; i < marqueeElementsDisplayed; i++) marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
